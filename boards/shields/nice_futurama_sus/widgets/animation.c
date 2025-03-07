3401dd6083dbf61a88315c4a305e1d7600cdabcb
whoop-t <tommymallis@gmail.com>
2025-03-06 18:06:32 -0800

    rename config and set default animation speed slower

--- boards/shields/nice_futurama_sus/widgets/animation.c
+++ boards/shields/nice_futurama_sus/widgets/animation.c
@@ -11,12 +11,12 @@ const lv_img_dsc_t *anim_imgs[] = {
 };
 
 void draw_animation(lv_obj_t *canvas) {
-#if IS_ENABLED(CONFIG_NICE_VIEW_GEM_ANIMATION)
+#if IS_ENABLED(CONFIG_NICE_FUTURAMA_SUS_ANIMATION)
     lv_obj_t *art = lv_animimg_create(canvas);
     lv_obj_center(art);
 
     lv_animimg_set_src(art, (const void **)anim_imgs, 16);
-    lv_animimg_set_duration(art, CONFIG_NICE_VIEW_GEM_ANIMATION_MS);
+    lv_animimg_set_duration(art, CONFIG_NICE_FUTURAMA_SUS_ANIMATION_MS);
     lv_animimg_set_repeat_count(art, LV_ANIM_REPEAT_INFINITE);
     lv_animimg_start(art);
 #else
